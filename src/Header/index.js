78b74cd3dce22f8f702c899ca87f079a4df92936
Audrey <a.comlancataria@epsi.fr>
2019-04-08 17:31:16 +0200

    Header component created

---
+++ src/Header/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => (
+  <header>
+    <h1>
+      Planning des employés
+      <span role="img" aria-label="calendrier">
+        📆
+      </span>
+    </h1>
+  </header>
+);
+
+export default Header;
