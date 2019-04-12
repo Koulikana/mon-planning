44d8fe81a177501f109449cfe45ee19c8c42c86c
Audrey <a.comlancataria@epsi.fr>
2019-04-13 00:09:43 +0200

    NavigationBar component created

--- src/App/index.js
+++ src/App/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../Header';
 import Calendar from '../Calendar';
+import NavigationBar from '../NavigationBar';
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <Header />
+        <NavigationBar />
         <Calendar />
       </div>
     );
---
+++ src/NavigationBar/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class NavigationBar extends React.Component {
+  render() {
+    return (
+      <div>
+        <nav class="mois">
+          <ul>
+            <li>Précédent</li>
+            <li>Suivant</li>
+          </ul>
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default NavigationBar;
