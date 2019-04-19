4408ac2de1dd1789b8c2b31c54a8947eab78ca95
Audrey <a.comlancataria@epsi.fr>
2019-04-19 20:25:32 +0200

    Colors modifyed

---
+++ src/TableHead/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+import { blue1, blue2 } from '../colors';
+
+export const StyledTd = styled.td({
+  backgroundColor: blue2,
+  color: blue1,
+});
+
+export const StyledTh = styled.th({
+  backgroundColor: blue1,
+  borderColor: blue2,
+});
